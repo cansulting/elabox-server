dae8fae255235f910f7496d08fa5deecda618973
ademcan <ademcan@ademcan.net>
2020-02-22 13:58:49 +0100

    Add check version for update

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const bodyParser = require("body-parser");
 const logger = require("morgan");
 // define port number
 const port = process.env.PORT || 3001;
+
+// define current version
+currentVersion = 1.2
  
 // instantiate express with the correct parameters
 app.use(logger('dev'));
@@ -19,6 +22,16 @@ const router = express.Router();
 // example of a GET method that returns a simple "Hello World"
 router.get('/', (req, res) => {
   res.send( "HELLO WORLD" );
+});
+
+router.post('/checkupdate', (req, res) => {
+  compVersion = req.body.compVersion
+  if (compVersion < currentVersion){
+    res.json({'update': true})
+  }
+  else {
+    res.json({'update': false})
+  }
 });
  
 // define the router to use
